a085dd3462687f7ebe411a923d714c9f35d02567
Yiji Huang <happybridge14@gmail.com>
2019-11-17 17:14:33 -0800

    Update data and data proxy

--- src/Proxy/Data.js
+++ src/Proxy/Data.js
@@ -1,5 +1,14 @@
 import items from "../data/items.json";
 import contactInfo from "../data/contactInfo.json";
+import categories from "../data/categories.json";
+
+let getIDMaker = (id, target)=> {
+  for (let i = 0; i < target.length; i++) {
+    if (target[i].id === id) {
+      return target[i];
+    }
+  }
+}
 
 let getItem = () => {
   return items;
@@ -7,5 +16,22 @@ let getItem = () => {
 let getContractInfo = () => {
   return contactInfo;
 };
+let getCategories = () => {
+  return categories;
+};
+let getItemById = (id) => {
+  return getIDMaker(id, items);
+}
+let getCategoryById = (id) => {
+  return getIDMaker(id, categories);
+}
+let getItemsByCategoryId = (id) => {
+  return items.filter((v)=>{
+    return v.id === id;
+  });
+}
+let search = (keyWord) => {
+  return "";
+};
 
-export {getItem, getContractInfo};
+export {getItem, getContractInfo, getCategories, getCategoryById, getItemById, getItemsByCategoryId, search};
---
+++ src/Widgets/Video.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const Video = (props) => {
+    return (
+      <div>
+        <iframe title={props.title} width="560" height="315" src={props.url} frameBorder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      </div>
+    )
+}
+
+Video.propTypes = {
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired
+};
+
+export default Video
