dd14f7d1efd0c9e9b74ba39c9187471e3cf8a808
Yiji Huang <happybridge14@gmail.com>
2019-11-24 19:33:58 -0800

    Optimize form widget. Optimize css.

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -2,104 +2,20 @@ import React, { Component } from "react";
 import styles from './Contact.module.css';
 import {getContractInfo} from '../Proxy/Data.js';
 import NavigationBar from "../Widgets/NavigationBar";
+import Form from "../Widgets/Form";
 
-const navbarPosition = [true, "contact"];
-const contactInfo = getContractInfo();
-const emailValidate = /^\S+@\S+\.\S+$/;
+const EMAIL_VALIDATOR = /^\S+@\S+\.\S+$/;
 
 export default class Contact extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      nameError: false,
-      emailError: false,
-      messageError: false,
-      isSubmitting: false,
-      isInitialStatus: true,
-      nameValue: "",
-      emailValue: "",
-      messageValue: ""
-    };
-  }
-  checkError = (target, value) => {
-    let hasError = false;
-    switch (target) {
-      case "name":
-      case "message":
-        hasError = value === "";
-        break;
-      case "email":
-        hasError = !this.validateEmail(value);
-        break;
-      default:
-        break;
-    }
-    return hasError;
-  }
-
-  renderError = (target) => {
-    let targetError = target + "Error";
-    let errorString = "";
-    switch (target) {
-      case "name":
-        errorString = "Please enter your first name.";
-        break;
-      case "email":
-        errorString = "Please enter a valid email address: yourname@example.com";
-        break;
-      case "message":
-        errorString = "Message field is required";
-        break;
-      default:
-        break;
-    }
-    return (<p className={`${styles.errorText} + " " + ${this.state[targetError] ? "" : styles.hiddenError}`}>{errorString}</p>);
-  }
-
-  handleChange = (e) => {
-    e.preventDefault();
-    let target = e.target.name;
-    let value = e.target.value;
-    let targetKey = target + "Value";
-    if (this.state[targetKey] !== value) {
-      let newState = {};
-      if (!this.state.isInitialStatus) {
-        let targetError = target + "Error";
-        newState[targetError] = this.checkError(target, value);
-      }
-      newState[targetKey] = value;
-      this.setState(newState);
-    }
-  }
-
-  validateEmail = (address) => {
-    return emailValidate.test(address);
-  }
-
-  onSubmit = async (e) => {
-    e.preventDefault();
-    let newState = {
-      isInitialStatus: false,
-      isSubmitting: true
-    };
-    ["name", "email", "message"].forEach((v) => {
-      let targetError = v + "Error";
-      let targetValue = v + "Value";
-      let errorResult = this.checkError(v, this.state[targetValue]);
-      newState[targetError] = errorResult;
-    });
-    this.setState(newState);
-    //// TODO:
-    // await something....
-    //////////////////////
-    this.setState({isSubmitting: false});
-    //// TODO:
-    // popup
+    this.navbarPosition = [true, "contact"];
+    this.contactInfo = getContractInfo();
   }
 
   createNodes = () => {
     let result = [];
-    contactInfo.forEach((v, i) => {
+    this.contactInfo.forEach((v, i) => {
       result.push(<h2 key={2*i}>{v.name}</h2>);
       result.push(<address key={2*i+1}>
         <a href={`mailto:${v.email}`} className={styles.mailto}>{v.email}</a>
@@ -108,10 +24,48 @@ export default class Contact extends Component {
     return result;
   }
 
+  onSubmit = async () => {
+    console.log("Contact.onSubmit");
+    return true;
+    // TODO:
+  }
+
+  formParam = {
+    dataSubmit: this.onSubmit,
+    items: [{
+      "id": "name",
+      "name": "Name",
+      "controlType": "input",
+      "type": "text",
+      "errorString": "Please enter your name.",
+      "validator": (value) => {
+        return value !== "";
+      }
+    }, {
+      "id": "email",
+      "name": "Email",
+      "controlType": "input",
+      "type": "text",
+      "errorString": "Please enter a valid email address: yourname@example.com.",
+      "validator": (value) => {
+        return EMAIL_VALIDATOR.test(value);
+      }
+    }, {
+      "id": "message",
+      "name": "Message",
+      "controlType": "textArea",
+      "type": "",
+      "errorString": "Message field is required.",
+      "validator": (value) => {
+        return value !== "";
+      }
+    }]
+  }
+
   render() {
     return (
         <div className={styles.contact}>
-          <NavigationBar positions={navbarPosition}/>
+          <NavigationBar positions={this.navbarPosition}/>
           <div className={styles.container}>
             <section className={styles.leftContainer}>
               <h1>
@@ -123,26 +77,7 @@ export default class Contact extends Component {
               <h1>
                 Leave a message
               </h1>
-              <form action="submit" onSubmit={this.onSubmit}>
-                <div className={styles.formContainer}>
-                  <label className={styles.label} htmlFor="name">Name</label>
-                  <div className={this.state.nameError ? styles.error : ""}>
-                    <input className={styles.input} type="text" name="name" id="name" value={this.state.nameValue} onChange={this.handleChange}/>
-                  </div>
-                  {this.renderError("name")}
-                  <label className={styles.label} htmlFor="email">E-mail</label>
-                  <div className={this.state.emailError ? styles.error : ""}>
-                    <input className={styles.input} type="text" name="email" id="email" value={this.state.emailValue} onChange={this.handleChange}/>
-                  </div>
-                  {this.renderError("email")}
-                  <label className={styles.label} htmlFor="message">Message</label>
-                  <div className={this.state.messageError ? styles.error : ""}>
-                    <textarea className={styles.textArea} id="message" name="message" rows="5" value={this.state.messageValue} onChange={this.handleChange}></textarea>
-                  </div>
-                  {this.renderError("message")}
-                  <button type="submit" disabled={this.state.nameError || this.state.emailError || this.state.messageError || this.state.isSubmitting} className={`${this.state.nameError || this.state.emailError || this.state.messageError || this.state.isSubmitting ? "disabled" : ""} baseButton ${styles.submitButton}`}>Submit</button>
-                </div>
-              </form>
+              <Form param={this.formParam}></Form>
             </section>
           </div>
         </div>
--- src/Widgets/Form.jsx
+++ src/Widgets/Form.jsx
@@ -1,27 +1,118 @@
 import React, { Component } from "react";
+import styles from './Form.module.css';
 
 export default class Form extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      submittedForm: null
-    };
+    this.state = this.getResetState();
+  }
+  checkError = (targetID, value) => {
+    return !this.state[targetID + "Validator"](value);
   }
 
-  onSubmit = async e => {
+  handleChange = (e) => {
     e.preventDefault();
-    const data = new FormData(e.target);
-    this.setState({ submittedForm: data });
+    let id = e.target.id;
+    let value = e.target.value;
+    let targetKey = id + "Value";
+    if (this.state[targetKey] !== value) {
+      let newState = {};
+      if (!this.state.isInitialStatus) {
+        let targetError = id + "Error";
+        newState[targetError] = this.checkError(id, value);
+      }
+      newState[targetKey] = value;
+      this.setState(newState);
+    }
+  }
+
+  onSubmit = async (e) => {
+    e.preventDefault();
+    let newState = {
+      isInitialStatus: false,
+    };
+    let hasError = false;
+    this.props.param.items.forEach((v) => {
+      let targetError = v.id + "Error";
+      let targetValue = v.id + "Value";
+      let errorResult = this.checkError(v.id, this.state[targetValue]);
+      if (errorResult) {
+        hasError = true;
+      }
+      newState[targetError] = errorResult;
+    });
+    if (!hasError) {
+      newState.isSubmitting = true;
+      this.setState(newState);
+      let result = await this.props.param.dataSubmit();
+      this.setState({isSubmitting: false});
+      if (result) {
+      //// TODO:
+      // popup success;
+      } else {
+      //// TODO:
+      // popup
+      // popup failure;
+      }
+      /////////////////
+      this.reset();
+      /////////////////
+    } else {
+      this.setState(newState);
+    }
+  }
+
+  getResetState = () => {
+    let state = {
+      isSubmitting: false,
+      isInitialStatus: true,
+    };
+    this.props.param.items.forEach((v) => {
+      state[v.id + "Error"] = false;
+      state[v.id + "Value"] = "";
+      state[v.id + "Validator"] = v.validator;
+    });
+    return state;
+  }
+  reset = () => {
+    this.setState(this.getResetState());
   };
 
+  createNodes = () => {
+    let nodes = [];
+    let index = 0;
+    this.props.param.items.forEach((v) => {
+      let errorKey = v.id + "Error";
+      let valueKey = v.id + "Value";
+      nodes.push(<label className={styles.label} htmlFor={v.id} key={index++}>{v.name}</label>);
+      let inputControl;
+      if (v.controlType === "input") {
+        inputControl = <input className={styles.input} type={v.type} name={v.id} id={v.id} value={this.state[valueKey]} onChange={this.handleChange}/>
+      } else if (v.controlType === "textArea") {
+        inputControl = <textarea className={styles.textArea} name={v.id} id={v.id} rows="5" value={this.state[valueKey]} onChange={this.handleChange}/>
+      }
+      nodes.push(<div className={this.state[errorKey] ? styles.error : ""} key={index++}>
+        {inputControl}
+      </div>);
+      nodes.push(<p className={`${styles.errorText} + " " + ${this.state[errorKey] ? "" : styles.hiddenError}`} key={index++}>{v.errorString}</p>)
+    });
+    return nodes;
+  }
+
+  hasError = () => {
+    return this.props.param.items.some(v => {
+      return this.state[v.id + "Error"];
+    });
+  }
+
   render() {
-    return this.state.submittedForm ? (
-      <div>Your form entry was {this.state.submittedForm.get("myText")}</div>
-    ) : (
-      <form onSubmit={this.onSubmit}>
-        <input type="text" name="myText" />
-        <input type="submit" value="Send it" />
+    return (
+      <form action="submit" onSubmit={this.onSubmit} className={styles.form}>
+        <div className={styles.formContainer}>
+          {this.createNodes()}
+          <button type="submit" disabled={this.hasError() || this.state.isSubmitting} className={`${this.hasError()  ? "disabled" : ""} baseButton ${styles.submitButton}`}>Submit</button>
+        </div>
       </form>
-    );
+    )
   }
 }
