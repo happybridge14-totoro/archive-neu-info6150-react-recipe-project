1062492f77a48e326b0a0bc8fd59b503dcecefa3
yiyiingit <jerryyiyi@126.com>
2019-12-03 15:28:01 -0800

    update styling

--- src/Pages/Detail.jsx
+++ src/Pages/Detail.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
+import {ulid} from 'ulid';
 import PropTypes from 'prop-types';
 import {getItemById} from "../Proxy/Data";
 import styles from "./Detail.module.css";
-import items from "../data/items.json";
 import ReactPlayer from "react-player";
 import NavigationBar from "../Widgets/NavigationBar"
         // <items = {Object.values(items)}>
 
-export default class Category extends Component {
+export default class Detail extends Component {
   constructor(props) {
     super(props);
     let detail = getItemById(this.props.id);
@@ -23,39 +23,59 @@ export default class Category extends Component {
       return;
     }
     return (
-      <article className = {styles.container}>
+      <article className = {styles.wholePage}>
         <NavigationBar positions={this.navbarPosition}/>
-        <div className = {styles.topleft}>
-          <h1 >{this.state.detail.title}</h1>
-          <h2>Rating: {this.state.detail.rating}</h2>
-          <h2>Time: {this.state.detail.time}</h2>
-        </div>
+        <section className = {styles.container}>
+            <div className = {styles.topleft}>
+              <h1 >{this.state.detail.title}</h1>
+              <h2>Rating: {this.state.detail.rating}</h2>
+              <h2>Time: {this.state.detail.time}</h2>
+            </div>
+            
+            {this.state.detail.videoURL !==""&&
+            <div > 
+              <ReactPlayer 
+                className = {styles.videoWrapper}
 
-        <div > 
-          <ReactPlayer
-            url = {this.state.detail.videoURL}
-            />
-        </div>
+                url = {this.state.detail.videoURL}
+                alt = {this.state.detail.shortName} dish video
+                />
+            </div>}
 
-        <div className = {styles.ingredients}>
-          <p className ={styles.sectionTitle}> Ingredients: </p>
-          {this.state.detail.ingredients.map((val,index) =>{
-            return <li key = {index}> {val} </li>
-          })}
-        </div>
+            {this.state.detail.videoURL ==""&&
+            <div > 
+              <img
+                className = {styles.imageWrapper}
+                src = {this.state.detail.imageURL}
+                alt = {this.state.detail.shortName} dish image
+                />
+            </div>}
 
-        <div className = {styles.directions}>
-          <p className ={styles.sectionTitle}> Directions: </p>
-          {this.state.detail.directions.map((val,index) =>{
-            return <ol> {index+1} : {val} </ol>
-          })}
-        </div>
+
+            <div className = {styles.ingredients}>
+              <p className ={styles.sectionTitle}> Ingredients: </p>
+              <ul> 
+              {this.state.detail.ingredients.map((val,index) =>{
+                return <li key={ulid()}> {val} </li>
+              })}
+              </ul>
+            </div>
+
+            <div className = {styles.directions}>
+              <p className ={styles.sectionTitle}> Directions: </p>
+              <ol>
+              {this.state.detail.directions.map((val,index) =>{
+                return <li key={ulid()}> {val} </li>
+              })}
+              </ol>
+            </div>
+        </ section>
 
       </article>
     );
   }
 }
 
-Category.propTypes = {
+Detail.propTypes = {
   id: PropTypes.string.isRequired
 };
